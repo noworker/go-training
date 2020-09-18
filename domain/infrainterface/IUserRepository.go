42719c6dbe7cc2721098e9c736c6b59c49c14cb4
ryo0 <nekonekosekai@gmail.com>
2020-09-18 19:49:34 +0900

    fix: user/userpassword切り分けをrepoに反映

--- domain/infrainterface/IUserRepository.go
+++ domain/infrainterface/IUserRepository.go
@@ -2,12 +2,11 @@ package infrainterface
 
 import (
 	"go_training/domain/model"
-	"go_training/infrastructure/table"
 )
 
 type IUserRepository interface {
 	Activate(userId model.UserId, password model.Password) error
-	CheckIfActivated(userId model.UserId, password model.Password) (bool, error)
-	GetUserByIdAndPassword(userId model.UserId, password model.Password) (table.User, error)
-	SaveUnactivatedNewUser(userId model.UserId, emailAddress model.EmailAddress, password model.Password) error
+	//CheckIfActivated(userId model.UserId, password model.Password) (bool, error)
+	//GetUserByIdAndPassword(userId model.UserId, password model.Password) (table.User, error)
+	CreateUnactivatedNewUser(userId model.UserId, emailAddress model.EmailAddress, password model.Password) error
 }
--- infrastructure/repository/UserRepository.go
+++ infrastructure/repository/UserRepository.go
@@ -18,48 +18,82 @@ func NewUserRepository(DB *gorm.DB) infrainterface.IUserRepository {
 }
 
 func (repository userRepository) Activate(userId model.UserId, password model.Password) error {
-	user := table.User{}
+	userPassword := table.UserPassword{}
 	conn := map[string]interface{} {
 		"user_id": userId,
 		"password": password,
-		"activated": true,
 	}
-	// アカウントの存在を予め確認したほうが良いかも……
-	result := repository.DB.Where(conn).Save(&user)
+	result := repository.DB.Where(conn).Find(&userPassword)
+	if err := result.Error; err != nil {
+		return err
+	}
+
+	user := table.User{
+		Activated: true,
+	}
+	conn2 := map[string]interface{} {
+		"user_id": userId,
+	}
+	result2 := repository.DB.Where(conn2).Save(&user)
+	if err := result2.Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//func (repository userRepository) CheckIfActivated(userId model.UserId, password model.Password) (bool, error) {
+//	user, err := repository.GetUserByIdAndPassword(userId, password)
+//	if err != nil {
+//		return false, err
+//	}
+//	return user.Activated, nil
+//}
+
+//func (repository userRepository) GetUserByIdAndPassword(userId model.UserId, password model.Password) (table.User, error) {
+//	user := table.User{}
+//	conn := map[string]interface{} {
+//		"user_id": userId,
+//		"password": password,
+//	}
+//	result := repository.DB.Where(conn).Find(&user)
+//	if err := result.Error; err != nil {
+//		return table.User{}, err
+//	}
+//	return user, nil
+//}
+
+func (repository userRepository) CreateUnactivatedNewUser(userId model.UserId, emailAddress model.EmailAddress, password model.Password) error {
+	err := repository.createUser(userId, emailAddress)
+	if err != nil {
+		return err
+	}
+	err = repository.createUserPassword(userId, password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repository userRepository) createUser(userId model.UserId, emailAddress model.EmailAddress) error {
+	user := table.User{}
+	conn := map[string]interface{} {
+		"user_id": userId,
+		"email_address": emailAddress,
+		"activated": false,
+	}
+	result := repository.DB.Where(conn).Create(&user)
 	if err := result.Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (repository userRepository) CheckIfActivated(userId model.UserId, password model.Password) (bool, error) {
-	user, err := repository.GetUserByIdAndPassword(userId, password)
-	if err != nil {
-		return false, err
-	}
-	return user.Activated, nil
-}
-
-func (repository userRepository) GetUserByIdAndPassword(userId model.UserId, password model.Password) (table.User, error) {
-	user := table.User{}
+func (repository userRepository) createUserPassword(userId model.UserId, password model.Password) error {
+	user := table.UserPassword{}
 	conn := map[string]interface{} {
 		"user_id": userId,
-		"password": password,
-	}
-	result := repository.DB.Where(conn).Find(&user)
-	if err := result.Error; err != nil {
-		return table.User{}, err
-	}
-	return user, nil
-}
-
-func (repository userRepository) SaveUnactivatedNewUser(userId model.UserId, emailAddress model.EmailAddress, password model.Password) error {
-	user := table.User{}
-	conn := map[string]interface{} {
-		"user_id": userId,
-		"email_address": emailAddress,
-		"password": password,
-		"activated": false,
+		"pass_word": password,
 	}
 	result := repository.DB.Where(conn).Create(&user)
 	if err := result.Error; err != nil {
