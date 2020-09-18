f3a774b818d3051d25a35dfaacca0c2e549a7e90
ryo0 <nekonekosekai@gmail.com>
2020-09-18 17:50:37 +0900

    feat: model.userPassword

--- domain/model/user.go
+++ domain/model/user.go
@@ -1,11 +1,10 @@
 package model
 
 type UserId string
-type Password HashedString
 type EmailAddress string
 
 type User struct {
 	UserId
-	Password
 	EmailAddress
+	Activated bool
 }
---
+++ domain/model/userPassword.go
@@ -0,0 +1,8 @@
+package model
+
+type Password HashedString
+
+type UserPassword struct {
+	UserId
+	Password,
+}
