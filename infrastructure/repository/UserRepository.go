72fbfa187066669a94b69d19b83a14cafbde0ca5
ryo0 <nekonekosekai@gmail.com>
2020-09-18 14:53:27 +0900

    fix: 初期化

--- infrastructure/repository/UserRepository.go
+++ infrastructure/repository/UserRepository.go
@@ -18,7 +18,7 @@ func NewUserRepository(DB *gorm.DB) infrainterface.IUserRepository {
 }
 
 func (repository UserRepository) Activate(userId model.UserId, password model.Password) error {
-	var user table.User{}
+	user := table.User{}
 	conn := map[string]interface{} {
 		"user_id": userId,
 		"password": password,
@@ -41,7 +41,7 @@ func (repository UserRepository) CheckIfActivated(userId model.UserId, password 
 }
 
 func (repository UserRepository) GetUserByIdAndPassword(userId model.UserId, password model.Password) (table.User, error) {
-	var user table.User{}
+	user := table.User{}
 	conn := map[string]interface{} {
 		"user_id": userId,
 		"password": password,
@@ -54,7 +54,7 @@ func (repository UserRepository) GetUserByIdAndPassword(userId model.UserId, pas
 }
 
 func (repository UserRepository) SaveUnactivatedNewUser(userId model.UserId, emailAddress model.EmailAddress, password model.Password) error {
-	var user table.User{}
+	user := table.User{}
 	conn := map[string]interface{} {
 		"user_id": userId,
 		"email_address": emailAddress,
