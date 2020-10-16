b36570c9564a8de248570af002803bf4fb43974e
ryo0 <nekonekosekai@gmail.com>
2020-10-16 14:45:33 +0900

    feat: error case test

--- web/handler/createUserHandler_test.go
+++ web/handler/createUserHandler_test.go
@@ -53,3 +53,43 @@ func TestCreateUserHandlerNoErrorCase(t *testing.T) {
 		t.Error(fmt.Sprintf("\nresult: %s\nexpected: %s", repo.Password, password))
 	}
 }
+
+func TestCreateUserHandlerErrorCase(t *testing.T) {
+	repo := repository.NewUserRepositoryMock(existingUserId)
+	handlers := InitHandler(initializer.Repositories{UserRepository: repo})
+	e := NewRouter(handlers)
+	form1 := make(url.Values)
+
+	userId := "abcde"
+	emailAddress := "abc@example.com"
+	password := "12345678"
+
+	form1.Set("user_id", userId)
+	form1.Set("email_address", emailAddress)
+	form1.Set("password", "111")
+
+	req1 := httptest.NewRequest(http.MethodPost, "/api/users", strings.NewReader(form1.Encode()))
+	req1.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec1 := httptest.NewRecorder()
+
+	e.ServeHTTP(rec1, req1)
+
+	if !assert.Equal(t, http.StatusBadRequest, rec1.Code) {
+		t.Error("error")
+	}
+
+	form2 := make(url.Values)
+	form2.Set("user_id", userId)
+	form2.Set("email_address", "aaaexample.com")
+	form2.Set("password", password)
+
+	req2 := httptest.NewRequest(http.MethodPost, "/api/users", strings.NewReader(form1.Encode()))
+	req2.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec2 := httptest.NewRecorder()
+
+	e.ServeHTTP(rec2, req2)
+
+	if !assert.Equal(t, http.StatusBadRequest, rec2.Code) {
+		t.Error("error")
+	}
+}
