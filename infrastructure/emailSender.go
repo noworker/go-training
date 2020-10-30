02b1ebb8dc92d762c812e762413bb312cee34e12
ryo0 <nekonekosekai@gmail.com>
2020-10-30 18:19:52 +0900

    fix:

--- infrastructure/emailSender.go
+++ infrastructure/emailSender.go
@@ -4,15 +4,6 @@ import (
 	"net/smtp"
 )
 
-type mail struct {
-	from     string
-	username string
-	password string
-	to       string
-	sub      string
-	msg      string
-}
-
 type EMailSender struct {
 	emailAddress string
 	password     string
@@ -23,10 +14,8 @@ func NewEmailSender(address, password string) EMailSender {
 }
 
 func (sender EMailSender) SendEmail(to string) error {
-	// func PlainAuth(identity, username, password, host string) Auth
 	auth := smtp.PlainAuth(sender.emailAddress, sender.emailAddress, sender.password, "smtp.gmail.com")
 
-	// func SendMail(addr string, a Auth, from string, to []string, msg []byte) error
 	err := smtp.SendMail("smtp.gmail.com:587", auth, sender.emailAddress, []string{to}, MessageBuilder(to))
 	if err != nil {
 		return err
