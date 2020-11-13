d799c176d318b1f9c28d4cc7e67384c6bf1e937d
ryo0 <nekonekosekai@gmail.com>
2020-11-13 20:56:28 +0900

    temp

---
+++ lib/jwt_lib/jwtChecker.go
@@ -0,0 +1,60 @@
+package jwt_lib
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"go_training/lib/errors"
+	"io/ioutil"
+)
+
+const (
+	UnexpectedSigningMethod errors.ErrorMessage = "unexpected signing method"
+	NotEvenAToken           errors.ErrorMessage = "not even a token"
+	Expired                 errors.ErrorMessage = "token is expired"
+	NotValidYet             errors.ErrorMessage = "token is not valid yet"
+	CanNotHandle            errors.ErrorMessage = "can not handle this token"
+)
+
+func Checker(jwtStr string) (bool, error) {
+	verifyBytes, err := ioutil.ReadFile(thisDir + "public.pem")
+	if err != nil {
+		println("hoge1")
+		return false, err
+	}
+
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(verifyBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	token, err := jwt.Parse(jwtStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			println("hoge3")
+			return nil, errors.CustomError{Message: UnexpectedSigningMethod}
+		} else {
+			return signKey, nil
+		}
+	})
+	if err != nil {
+		println("hoge4")
+		return false, err
+	}
+
+	if token.Valid {
+		return true, nil
+	} else if ve, ok := err.(*jwt.ValidationError); ok {
+		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+			println("hoge5")
+			return false, errors.CustomError{Message: NotEvenAToken}
+		} else if ve.Errors&(jwt.ValidationErrorExpired) != 0 {
+			println("hoge6")
+			return false, errors.CustomError{Message: Expired}
+		} else if ve.Errors&(jwt.ValidationErrorNotValidYet) != 0 {
+			return false, errors.CustomError{Message: NotValidYet}
+		} else {
+			return false, errors.CustomError{Message: CanNotHandle}
+		}
+	} else {
+		println("hoge7")
+		return false, errors.CustomError{Message: CanNotHandle}
+	}
+}
---
+++ lib/jwt_lib/jwtGenerator.go
@@ -0,0 +1,29 @@
+package jwt_lib
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	mdate "github.com/matsuri-tech/date-go"
+	"io/ioutil"
+)
+
+const thisDir = "/home/ryo/matsuri/go-training/"
+
+func Generator(userId string) (string, error) {
+	signBytes, err := ioutil.ReadFile(thisDir + "private.pem")
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userId,
+		"exp":     mdate.GetToday().PlusNDay(1).Unix(),
+	})
+
+	tokenString, err := token.SignedString(signBytes)
+	if err != nil {
+		println("hoge3")
+		return "", err
+	}
+
+	return tokenString, nil
+}
